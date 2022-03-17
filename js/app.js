d501786ababc9137e9ba3790122783836dee4559
Pankaj Chaudhary <67068794+Pnkj124@users.noreply.github.com>
2022-03-17 05:36:44 +0100

    add layouts

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        projects: [],
+    },
+    mounted() {
+        fetch('https://gh-pinned-repos.egoist.sh/?username=Pnkj124')
+            .then(res => res.json())
+            .then(res => {
+                for (const repo of res) {
+                    this.projects.push({
+                        project_icon: "./img/github.svg",
+                        project_title: repo.repo,
+                        project_description: repo.description,
+                        url: repo.link,
+                        language: repo.language,
+                        stars: repo.stars,
+                        forks: repo.forks
+                    })
+                }
+            });
+    }
+})
