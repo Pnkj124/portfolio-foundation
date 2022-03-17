bdf1fc968c91b1b19ffa252261b051eb3002735b
Pankaj Chaudhary <67068794+Pnkj124@users.noreply.github.com>
2022-03-17 06:16:42 +0100

    update package manage to npm and fix js packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var $ = require('gulp-load-plugins')();
+var autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
@@ -18,9 +20,22 @@ function sass() {
     .pipe($.postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 };
+
+function js() {
+  return gulp.src([
+    './node_modules/vue/dist/vue.min.js',
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/foundation-sites/dist/js/foundation.js',
+    'js/*.js'
+  ])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+};
+
 
 function serve() {
   browserSync.init({
@@ -28,9 +43,11 @@ function serve() {
   });
 
   gulp.watch("scss/*.scss", sass);
+  gulp.watch("js/*.js", js);
   gulp.watch("*.html").on('change', browserSync.reload);
 }
 
 gulp.task('sass', sass);
+gulp.task('js', js);
 gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series('sass', serve));
+gulp.task('default', gulp.series(['sass', 'js'], serve));
